550bdc24a92627ac0ecc9a10cbfdc1c952791039
Yves Gille <ygille@student.42lyon.fr>
2024-11-20 19:25:57 +0100

    Renvoi bien NULL après la fin du fichier mais leaks

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ygille <ygille@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 17:23:06 by ygille            #+#    #+#             */
-/*   Updated: 2024/11/20 19:11:37 by ygille           ###   ########.fr       */
+/*   Updated: 2024/11/20 19:23:46 by ygille           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,12 @@ char	*get_next_line(int fd)
 	char		*res;
 	int			i;
 	char		tmp[BUFFER_SIZE + 1];
+	static int	state = 1;
 
 	tmp[0] = '\0';
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
 		return (NULL);
-	res = read_buff(buff, fd);
+	res = read_buff(buff, fd, &state);
 	i = 0;
 	while (buff[i] != '\0' && buff[i] != '\n')
 		i++;
@@ -39,18 +40,18 @@ char	*get_next_line(int fd)
 	return (res);
 }
 
-char	*read_buff(char *buff, int fd)
+char	*read_buff(char *buff, int fd, int *state)
 {
-	int		state;
 	char	*line;
 
 	line = NULL;
-	state = 1;
-	while (ft_strchr(buff, '\n') == NULL && state > 0)
+	if (*state == 0 && buff[0] == '\0')
+		return (NULL);
+	while (ft_strchr(buff, '\n') == NULL && *state > 0)
 	{
-		state = read(fd, buff, BUFFER_SIZE);
-		buff[state] = '\0';
-		if (ft_strchr(buff, '\n'))
+		*state = read(fd, buff, BUFFER_SIZE);
+		buff[*state] = '\0';
+		if (ft_strchr(buff, '\n') || state == 0)
 			return (extract_line(line, buff));
 		else
 			line = ft_strjoin(line, buff);
@@ -69,7 +70,6 @@ char	*extract_line(char *line, char *buff)
 	if (buff[i] == '\n')
 		i++;
 	res = malloc(sizeof(char) * ft_strlen(line) + i + 1);
-	printf("size = %i\n", ft_strlen(line) + i + 1);
 	if (res == NULL)
 		return (NULL);
 	ft_strlcat(res, line, ft_strlen(line) + 1);
@@ -79,25 +79,26 @@ char	*extract_line(char *line, char *buff)
 	return (res);
 }
 
-#include <fcntl.h>
-#include <stdio.h>
-int	main(void)
-{
-	int		fd;
-	int		i;
-	char	*str;
+// #include <fcntl.h>
+// #include <stdio.h>
+// int	main(int argc, char *argv[])
+// {
+// 	int		fd;
+// 	int		i;
+// 	char	*str;
 
-	fd = open("files/lines_around_10.txt", O_RDONLY);
-	str = get_next_line(fd);
-	i = 1;
-	while (i < 10)
-	{
-		printf("Line %d = |%s|\n", i, str);
-		free(str);
-		str = get_next_line(fd);
-		i++;
-	}
-	printf("End");
-	close(fd);
-	return (0);
-}
+// 	(void) argc;
+// 	fd = open(argv[1], O_RDONLY);
+// 	str = get_next_line(fd);
+// 	i = 1;
+// 	while (str)
+// 	{
+// 		printf("Line %d = |%s|\n", i, str);
+// 		free(str);
+// 		str = get_next_line(fd);
+// 		i++;
+// 	}
+// 	printf("End");
+// 	close(fd);
+// 	return (0);
+// }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: ygille <ygille@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 17:23:13 by ygille            #+#    #+#             */
-/*   Updated: 2024/11/20 19:02:33 by ygille           ###   ########.fr       */
+/*   Updated: 2024/11/20 19:17:52 by ygille           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@
 
 //get_next_line.c
 char	*get_next_line(int fd);
-char	*read_buff(char *buff, int fd);
+char	*read_buff(char *buff, int fd, int *state);
 char	*extract_line(char *line, char *buff);
 
 //get_next_line_utils.c
